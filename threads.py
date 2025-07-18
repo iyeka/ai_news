c36d8151b78fff1e873645503fefbc76e01e8300
iyeka <gayeadang@naver.com>
2025-07-18 11:50:00 +0900

    Threads_complete_one_by_one

--- threads.py
+++ threads.py
@@ -15,6 +15,7 @@ class rss_generator():
         self.rss_generator_url = "https://rss.app/new-rss-feed"
         self.myfeeds = "https://rss.app/myfeeds"
 
+    # Save and load login information of RSS.app.
     async def save_cookies(self, context):
         cookies = await context.storage_state()
         with open(self.cookie_file, "w") as file:
@@ -26,12 +27,10 @@ class rss_generator():
                 return json.load(file)
         return None
 
-    async def get_rss_urls(self, usernames:Iterable) -> list:
-        rss_urls = []
-
+    async def get_rss_url(self, username:str) -> str:
         async with async_playwright() as p:
             if Path(self.cookie_file).exists():
-                print("Login info found!")
+                print("Login info found🫡")
                 browser = await p.chromium.launch()
                 context = await browser.new_context(storage_state=await self.load_cookies())
                 page = await context.new_page()
@@ -42,34 +41,36 @@ class rss_generator():
 
                 # If cookies don't exist, login manually once
                 await page.goto(self.rss_login_url)
-                input("log in manually, then press Enter here.")
-                await self.save_cookies(context)
-
-            for username in usernames:
-                profile_url = f"https://www.threads.net/@{username}"
-                await page.goto(self.rss_generator_url)
-
-                search_input = page.locator("input.MuiAutocomplete-input")
-                await search_input.fill(profile_url)
-                await expect(search_input).to_have_value(profile_url)
-
-                await page.click("button[type='submit']")
-                print(f"making url for {username}... please wait.")
-
+                input("Log in manually, then press Enter here.")
                 try:
-                    await page.get_by_role("button", name="Save Feed").click()
-                except TimeoutError:
-                    alert = page.locator("div.MuiAlert-message")
-                    if await alert.is_visible():
-                        print(alert.text_content())
-                        break
+                    await page.wait_for_url("https://rss.app/myfeeds")
+                    await self.save_cookies(context)
+                except:
+                    print(f"❌ Login failed. Current URL: {page.url}")
 
-                rss_url = await page.locator("input.Mui-readOnly").get_attribute("value")
-                rss_urls.append(rss_url)
+            profile_url = f"https://www.threads.net/@{username}"
+            await page.goto(self.rss_generator_url)
+
+            search_input = page.locator("input.MuiAutocomplete-input")
+            await search_input.fill(profile_url)
+            await expect(search_input).to_have_value(profile_url)
+
+            await page.click("button[type='submit']")
+            print(f"making url for {username}... please wait.")
+
+            try:
+                await page.get_by_role("button", name="Save Feed").click()
+            except TimeoutError:
+                alert = page.locator("div.MuiAlert-message")
+                if await alert.is_visible():
+                    alert_message = await alert.text_content()
+                    raise SystemExit(alert_message)
+
+            rss_url = await page.locator("input.Mui-readOnly").get_attribute("value")
                 
             await browser.close()
         
-        return rss_urls
+        return rss_url
     
     async def delete_feed(self):
         async with async_playwright() as p:
@@ -78,29 +79,37 @@ class rss_generator():
             page = await context.new_page()
 
             await page.goto(self.myfeeds)
-            await page.click("button[ga='feed-menu]")
-            await page.click("li[ga='menu-delete-feed']")
+            await page.click('button[ga="feed-menu"]')
+            await page.click('li[ga="menu-delete-feed"]')
+
+            heading = page.locator("h2", has_text="delete")
+            await heading.wait_for()
+            await page.click('button[ga="feed-delete-submit"]')
+            await page.wait_for_timeout(2000)
+            print("feed deleting succeed.")
 
             await browser.close()
 
-def get_posts(urls:list) -> list[dict]:
+def get_posts(url:str) -> list[dict]:
     def extract_text(html):
-            soup = BeautifulSoup(html, "html.parser")
-            text = soup.get_text(separator=" ", strip=True)
-            return text.replace("\n", " ")
+        soup = BeautifulSoup(html, "html.parser")
+        text = soup.get_text(separator=" ", strip=True)
+        return text.replace("\n", " ")
 
     posts=[]
-    for url in urls:
-        print(f"parsing {url}...")
-        feed = feedparser.parse(url)
-        for entry in feed.entries:
-            post = dict(
+    print(f"parsing {url}...")
+    feed = feedparser.parse(url)
+    for entry in feed.entries:
+        summary = entry.get("summary")
+        if not summary:
+            continue
+        post = dict(
             date = entry.get("published"),
             author = entry.get("author"),
-            content = extract_text(entry.get("summary")),
+            content = extract_text(summary),
             link = entry.get("link"),
-            )
-            posts.append(post)
+        )
+        posts.append(post)
 
     return posts
 
@@ -109,6 +118,7 @@ def get_base_url(url):
     return match.group(1)
 
 async def main(users):
-    urls = await rss_generator().get_rss_urls(users)
-    utils.BaseSave(sheet_name=config.THREADS_SHEET).duplicated_or_save(urls, get_posts, get_base_url)
-    await rss_generator().delete_feed()
+    for user in users:
+        url = await rss_generator().get_rss_url(user)
+        utils.BaseSave(sheet_name=config.THREADS_SHEET).duplicated_or_save(url, get_posts, get_base_url)
+        await rss_generator().delete_feed()
